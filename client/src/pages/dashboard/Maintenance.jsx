13821c8b195bfb31d0f1bc8b99f21e7e90c252aa
LabanMugutu <labanmugutu@gmail.com>
2026-01-15 15:49:45 +0300

    Fixes imports error

--- client/src/pages/dashboard/Maintenance.jsx
+++ client/src/pages/dashboard/Maintenance.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import DashboardLayout from '../../../layouts/DashboardLayout';
-import api from '../../../api/axios';
+import DashboardLayout from '../../layouts/DashboardLayout';
+import api from '../../api/axios';
 import { FaTools, FaPlus, FaCheckCircle, FaExclamationCircle, FaSpinner, FaHome, FaLock } from 'react-icons/fa';
 
 const Maintenance = () => {
